1ab8f96027c8440323cc278dc96520c99b36d90b
Linah-alkhurayyif <73877590+Linah-alkhurayyif@users.noreply.github.com>
2021-04-08 11:12:19 +0300

    add Logo image

--- lib/Logopage.dart
+++ lib/Logopage.dart
@@ -8,6 +8,23 @@ class Logopage extends StatefulWidget {
 class _LogopageState extends State<Logopage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          backgroundImage(),
+        ],
+      ),
+    );
+  }
+
+  Widget backgroundImage() {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("images/Coffee_Code_PLogo.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
 }
